8106214c4f676c1ea6c1b30a250776564012cf82
Mohamed Elkholy <mkh117@gmail.com>
2014-08-31 01:51:41 +0300

    Added internationalization strings

--- lib/vagrant-hosts-provisioner.rb
+++ lib/vagrant-hosts-provisioner.rb
@@ -1,10 +1,18 @@
-require 'vagrant-hosts-provisioner/plugin'
-require 'vagrant-hosts-provisioner/version'
+require "pathname"
+require "vagrant-hosts-provisioner/plugin"
+require "vagrant-hosts-provisioner/version"
 
 module VagrantPlugins
   module HostsProvisioner
+    # This returns the path to the source of this plugin.
+    #
+    # @return [Pathname]
     def self.source_root
       @source_root ||= Pathname.new(File.expand_path('../../', __FILE__))
     end
+
+    # This initializes the internationalization strings.
+    I18n.load_path << File.expand_path("locales/en.yml", self.source_root)
+    I18n.reload!
   end
 end
